34a805349625ba71046ecdcb2449cffeb87b5f6d
xiaotuan <583168425@qq.com>
2020-06-23 11:41:34 +0800

    更新第10章源代码

--- Chapter10/UseMongoDB2.js
+++ Chapter10/UseMongoDB2.js
@@ -4,3 +4,59 @@ const url = 'mongodb://localhost:27017'
 
 const dbName = 'exampleDb'
 
+MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client) {
+  if (!err) {
+    const col = client.db(dbName).collection('widgets')
+    // 删除所有 widgets 文档
+    col.deleteMany(null, function (err, result) {
+      if (!err) {
+        // 创建四条记录
+        var widget1 = {
+          id: 1,
+          title: 'First Great widget',
+          desc: 'greatest widget of all',
+          price: 14.99, 
+          type: 'A' 
+        }
+        var widget2 = {
+          id: 2,
+          title: 'Second Great widget',
+          desc: 'second greatest widget of all',
+          price: 29.99, 
+          type: 'A'
+        }
+        var widget3 = {
+          id: 3,
+          title: 'third widget', 
+          desc: 'third widget',
+          price: 45.00,
+          type: 'B'
+        }
+        var widget4 = {
+          id: 4, 
+          title: 'fourth widget', 
+          desc: 'fourth widget',
+          price: 60.00,
+          type: 'B'
+        }
+        col.insertMany([widget1, widget2, widget3, widget4], function (err, result) {
+          if (err) {
+            console.log(err)
+          } else {
+            // 返回所有文档
+            col.find().toArray(function (err, docs) {
+              console.log(docs)
+
+              // 关闭数据库连接
+              client.close()
+            })
+          }
+        })
+      } else {
+        console.log(err)
+      }
+    })
+  } else {
+    console.log(err)
+  }
+})
